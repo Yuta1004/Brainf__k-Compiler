ec357bb765c8a1475c2a9bcb6e7cee134aa7e802
NakagamiYuta <guguru0014@gmail.com>
2019-08-31 01:12:37 +0900

    [add] プログラムをファイルから読み込んで実行するようにした

--- main.go
+++ main.go
@@ -2,11 +2,21 @@ package main
 
 import (
 	"brainfOOk-compiler/asm"
+	"brainfOOk-compiler/common"
 	"brainfOOk-compiler/parse"
+	"flag"
+	"strings"
 )
 
 func main() {
+	flag.Parse()
+	programFile := flag.Arg(0)
+	if programFile == "" {
+		common.Error("引数が少なすぎます!")
+	}
+	program := strings.Split(common.ReadFile(programFile), "\n")[0]
+
 	asm.Header()
-	parse.Parse("++++")
+	asm.Body(parse.Parse(program))
 	asm.Footer()
 }
