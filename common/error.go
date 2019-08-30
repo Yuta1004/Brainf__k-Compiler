491dfbe926a0b45bc90a637533cca4c45f476b65
NakagamiYuta <guguru0014@gmail.com>
2019-08-31 00:57:47 +0900

    [add] 標準エラー出力へメッセージを出力する関数を作成

--- common/error.go
+++ common/error.go
@@ -6,6 +6,12 @@ import (
 	"strings"
 )
 
+// Error : エラーメッセージを出力する
+func Error(msg string) {
+	fmt.Fprintf(os.Stderr, msg+"\n")
+	os.Exit(1)
+}
+
 // ErrorWithPos : エラー箇所をメッセージとともに表示
 func ErrorWithPos(base, msg string, pos int) {
 	fmt.Fprintf(os.Stderr, base+"\n")
