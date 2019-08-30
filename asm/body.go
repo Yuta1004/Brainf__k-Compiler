6afd7b3968eb6c5309b60c0c94a707afe8d9290d
NakagamiYuta <guguru0014@gmail.com>
2019-08-31 00:59:00 +0900

    [add] +, -記号をコンパイル出来るようになった

---
+++ asm/body.go
@@ -0,0 +1,30 @@
+package asm
+
+import (
+	"brainfOOk-compiler/common"
+	"brainfOOk-compiler/parse"
+	"fmt"
+)
+
+// Body : ProgramInfoListをアセンブリに変換して出力する
+func Body(programItemList *[]parse.ProgramItem) {
+	pointerPos := 0
+
+	for _, programItem := range *programItemList {
+		// +, -
+		if programItem.Type == parse.ControlPointer {
+			fmt.Printf("		add rbx, %d\n", programItem.Value)
+			pointerPos += programItem.Value
+			checkMinusPointer(pointerPos)
+			continue
+		}
+
+		common.Error("不明なエラー")
+	}
+}
+
+func checkMinusPointer(pointerPos int) {
+	if pointerPos < 0 {
+		common.Error("ポインタが負の値を取りました")
+	}
+}
