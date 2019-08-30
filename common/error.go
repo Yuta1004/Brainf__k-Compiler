754113b4f40ef6d18e9472051570240bd9f665ae
NakagamiYuta <guguru0014@gmail.com>
2019-08-31 00:32:56 +0900

    [add] エラー文は標準エラー出力に出すように

--- common/error.go
+++ common/error.go
@@ -8,7 +8,7 @@ import (
 
 // ErrorWithPos : エラー箇所をメッセージとともに表示
 func ErrorWithPos(base, msg string, pos int) {
-	fmt.Println(base)
-	fmt.Println(strings.Repeat(" ", pos) + " : " + msg)
+	fmt.Fprintf(os.Stderr, base+"\n")
+	fmt.Fprintf(os.Stderr, "%s↑ %s\n", strings.Repeat(" ", pos), msg)
 	os.Exit(1)
 }
