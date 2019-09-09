71a3c9f9629234842ff6bc08bd88842a83e75004
NakagamiYuta <guguru0014@gmail.com>
2019-09-10 00:57:56 +0900

    [update] asm.Body()をProgramItemの連結リストに対応させた

--- asm/body.go
+++ asm/body.go
@@ -7,15 +7,19 @@ import (
 )
 
 // Body : ProgramInfoListをアセンブリに変換して出力する
-func Body(programItemList *[]parse.ProgramItem) {
+func Body(programItemTop *parse.ProgramItem) {
 	fmt.Println("body:")
 	pointerPos := 0
+	loopCount := 0
+	loopStack := make([]int, 0)
 
-	for _, programItem := range *checkLoopPair(programItemList) {
+	programItem := programItemTop
+	for programItem.Next != nil {
 		// >, <
 		if programItem.Type == parse.ControlPointer {
 			pointerPos += programItem.Value
 			checkMinusPointer(pointerPos)
+			programItem = programItem.Next
 			continue
 		}
 
@@ -24,26 +28,38 @@ func Body(programItemList *[]parse.ProgramItem) {
 			fmt.Println("		mov rdx, rbp")
 			fmt.Printf("		sub rdx, %d\n", pointerPos*8+8)
 			fmt.Printf("		add byte ptr [rdx], %d\n", programItem.Value)
+			programItem = programItem.Next
 			continue
 		}
 
 		// [
 		if programItem.Type == parse.LoopStart {
+			loopStack = append(loopStack, loopCount)
+			loopCount++
 			fmt.Println("		mov rdx, rbp")
 			fmt.Printf("		sub rdx, %d\n", pointerPos*8+8)
 			fmt.Println("		cmp byte ptr [rdx], 0")
-			fmt.Printf("		je __loop_end_%d\n", programItem.Value)
-			fmt.Printf("__loop_start_%d:\n", programItem.Value)
+			fmt.Printf("		je __loop_end_%d\n", loopCount-1)
+			fmt.Printf("__loop_start_%d:\n", loopCount-1)
+			programItem = programItem.Next
 			continue
 		}
 
 		// ]
 		if programItem.Type == parse.LoopEnd {
+			loopID := -1
+			if len(loopStack) > 0 {
+				loopID = loopStack[len(loopStack)-1]
+				loopStack = loopStack[:len(loopStack)-1]
+			} else {
+				common.Error("[, ]の対応が正しくありません")
+			}
 			fmt.Println("		mov rdx, rbp")
 			fmt.Printf("		sub rdx, %d\n", pointerPos*8+8)
 			fmt.Println("		cmp byte ptr [rdx], 0")
-			fmt.Printf("		jne __loop_start_%d\n", programItem.Value)
-			fmt.Printf("__loop_end_%d:\n", programItem.Value)
+			fmt.Printf("		jne __loop_start_%d\n", loopID)
+			fmt.Printf("__loop_end_%d:\n", loopID)
+			programItem = programItem.Next
 			continue
 		}
 
@@ -55,6 +71,7 @@ func Body(programItemList *[]parse.ProgramItem) {
 			fmt.Printf("		sub rsi, %d\n", pointerPos*8+8) // (ポインタ設定)
 			fmt.Println("		mov rdx, 1")                   // 第3引数 : nbyte
 			fmt.Println("		syscall")
+			programItem = programItem.Next
 			continue
 		}
 
@@ -66,6 +83,7 @@ func Body(programItemList *[]parse.ProgramItem) {
 			fmt.Printf("		sub rsi, %d\n", pointerPos*8+8)
 			fmt.Println("		mov rdx, 1")
 			fmt.Println("		syscall")
+			programItem = programItem.Next
 			continue
 		}
 
@@ -82,40 +100,4 @@ func checkMinusPointer(pointerPos int) {
 	if pointerPos < 0 {
 		common.Error("ポインタが負の値を取りました")
 	}
-}
-
-func checkLoopPair(programItemList *[]parse.ProgramItem) *[]parse.ProgramItem {
-	loopCount := 0
-	loopStack := make([]int, 0)
-	newProgramItemList := make([]parse.ProgramItem, 0)
-
-	// [, ]を対応づけていく
-	for _, programItem := range *programItemList {
-		// Start
-		if programItem.Type == parse.LoopStart {
-			loopStack = append(loopStack, loopCount)
-			programItem := parse.ProgramItem{Type: parse.LoopStart, Value: loopCount}
-			newProgramItemList = append(newProgramItemList, programItem)
-			loopCount++
-			continue
-		}
-		// End
-		if programItem.Type == parse.LoopEnd {
-			if len(loopStack) == 0 {
-				common.Error("[, ]の対応が正しくありません")
-			}
-			loopStart := loopStack[len(loopStack)-1]
-			loopStack = loopStack[:len(loopStack)-1]
-			programItem := parse.ProgramItem{Type: parse.LoopEnd, Value: loopStart}
-			newProgramItemList = append(newProgramItemList, programItem)
-			continue
-		}
-		// Others
-		newProgramItemList = append(newProgramItemList, programItem)
-	}
-
-	if len(loopStack) != 0 {
-		common.Error("[, ]の対応が正しくありません")
-	}
-	return &newProgramItemList
 }
